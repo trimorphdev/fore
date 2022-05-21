b97e829fce05b7ccea74e76be7c40744171ed7ac
Zack <trimorphyt@gmail.com>
2022-05-20 20:01:21 -0500

    :sparkles: add native component `head`

---
+++ fore-frontend/src/component/native/head.rs
@@ -0,0 +1,20 @@
+//! The HTML `head` element.
+n
+use crate::{AttributeMap, Children, Component, DOMElement, HTMLElement};
+
+/// The HTML `head` element as a component.
+#[derive(Clone, Debug, PartialEq)]
+pub struct Head {}
+
+impl Component for Head {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn render(&self, attrs: AttributeMap, children: Children) -> HTMLElement {
+        let mut element = HTMLElement::new("head".into());
+        *element.get_attributes_mut() = attrs;
+        *element.get_children_mut() = children;
+        element
+    }
+}
--- fore-frontend/src/component/native/mod.rs
+++ fore-frontend/src/component/native/mod.rs
@@ -1,9 +1,11 @@
 //! Native HTML elements as components.
 
 mod div;
+mod head;
 mod p;
 mod span;
 
 pub use div::Div;
+pub use head::Head;
 pub use p::P;
 pub use span::Span;
